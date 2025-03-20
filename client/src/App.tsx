bf9826f7bdecf1291183d2400ff8852eb8f5d6d6
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-03-20 19:54:29 +0000

    Remove SubjectDiagramsPage component and its route; clean up App component imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 
 import './App.css'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route,  Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import UploadForm from './pages/UploadPage'
 import ViewAllImages from './pages/ImageViewerPage'
 import Auth from './pages/Auth'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SubjectDiagramsPage from './pages/categories'
+
 
 const queryClient = new QueryClient();
 function App() {
@@ -21,7 +21,7 @@ function App() {
         <Route path="/signup" element={<Auth isSignUp= {true} />} />
         <Route path="/upload" element={<UploadForm />} />
         <Route path="/imagecollection" element={<ViewAllImages />} />
-        <Route path="/categories" element={<SubjectDiagramsPage />} />
+
       </Routes>
 
     </QueryClientProvider>
