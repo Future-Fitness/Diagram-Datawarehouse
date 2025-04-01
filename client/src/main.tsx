3cea01fdd42ef13e28641b80e8b690a2fc075395
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-03-31 18:33:52 -0700

    Update Vite configuration and BrowserRouter basename for GitHub Pages deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Diagram-Datawarehouse'>
     <App />
     <ToastContainer />
     </BrowserRouter>
