b8aeb071b9dc5574fbd7614584a8155c2e172a58
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-03-31 18:27:25 -0700

    Update Vite configuration for GitHub Pages deployment; add homepage and deploy scripts in package.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/datadiagram/',
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    // Ensure proper MIME types for assets
-    assetsInlineLimit: 0,
-  },
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/Diagram-Datawarehouse/', // this matches your GitHub Pages path
 })
